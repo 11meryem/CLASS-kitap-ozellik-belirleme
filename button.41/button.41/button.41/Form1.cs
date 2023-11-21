b222d4f1f5cc165de530ed45ca1d281b39d37b2a
11meryem <145452149+11meryem@users.noreply.github.com>
2023-11-21 11:48:20 +0300

    Add files via upload

---
+++ button.41/button.41/button.41/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button._41
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kitap kitap = new Kitap();
+            kitap.ad = "Büşra'nın çıkmadığı mutfağı";
+            kitap.yazar = " Köylü Büşra";
+            kitap.yayınEvi = "Büşra'nın evi";
+            kitap.sayfaSayisi = 1000;
+            kitap.cikisYili = 2020;
+            label1.Text = kitap.ad +"   "+kitap.yayınEvi+"   " +kitap.yazar+"   " +kitap.cikisYili+"   "+kitap.sayfaSayisi;
+
+        }
+    }
+}
---
+++ button.41/button.41/button.41/Kitap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace button._41
+{
+    internal class Kitap
+    {
+        public string ad;
+        public string yazar;
+        public string yayınEvi;
+        public int sayfaSayisi;
+        public int cikisYili;
+    }
+}
